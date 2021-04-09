697138c0b0b22215df205b54aeb697e9de2f3c51
Raunak Mantri <mantrir20@gmail.com>
2021-04-10 02:32:36 +0530

    Login and Registration Page

--- DBMS_PROJECT/src/Project.java
+++ DBMS_PROJECT/src/Project.java
@@ -4,33 +4,9 @@ import java.io.*;
 import java.util.*;
 import java.applet.*;
 import java.awt.*;
-public class Project {
-	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost:3306/dbms_proj";
-	static final String USER = "root";
-	   static final String PASS = "";
 
-	public static void main(String[] args)
-	{
-		Connection conn = null;
-		   Statement stmt = null;
-		   try {
-		      Class.forName(JDBC_DRIVER);
-		      System.out.println("Connecting to a selected database...");
-		      conn = DriverManager.getConnection(DB_URL, USER, PASS);
-		      stmt=conn.createStatement();
-		      System.out.println("Connected database successfully...");
-		      String sql = "SELECT * FROM moviedetail;";
-		      ResultSet val = stmt.executeQuery(sql);
-		      while(val.next())
-		      {
-		    	  System.out.println(val.getString(2));
-		      }
-		   }
-		   catch(Exception se)
-		   {
-			   se.printStackTrace();
-		   }
-		   //comment
+public class Project {
+	public static void main(String[] args){
+		new mainPage().setVisible(true);
 	}
 }
---
+++ DBMS_PROJECT/src/databaseConnect.java
@@ -0,0 +1,29 @@
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+
+public class databaseConnect{
+    
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/dbms_proj";
+	static final String USER = "root";
+	static final String PASS = "krish@08";
+    
+	public static Connection dbconnect(){ 
+	
+		Connection conn=null;
+	    try{
+	    	Class.forName(JDBC_DRIVER);
+	    	System.out.println("Connecting to a selected database\n");
+	    	conn = DriverManager.getConnection(DB_URL, USER, PASS);
+	    	System.out.println("Connected database successfully\n");
+	        return conn;
+	    
+	    }catch(Exception e){
+	    	
+	        JOptionPane.showMessageDialog(null ,e);
+	        System.out.println("Error Connecting to database\n");
+	        return null;
+	    	}
+	    }
+}
