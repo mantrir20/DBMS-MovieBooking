c047120427f77829c41e984ddb34f3b0d3044b4b
Sahitya02 <64304879+Sahitya02@users.noreply.github.com>
2021-04-09 20:24:34 +0530

    First commit

---
+++ DBMS_PROJECT/src/Project.java
@@ -0,0 +1,34 @@
+import java.sql.*;
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+import java.applet.*;
+import java.awt.*;
+public class Project {
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/dbms_proj";
+	static final String USER = "root";
+	   static final String PASS = "";
+
+	public static void main(String[] args)
+	{
+		Connection conn = null;
+		   Statement stmt = null;
+		   try {
+		      Class.forName("Project");
+		      System.out.println("Connecting to a selected database...");
+		      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		      System.out.println("Connected database successfully...");
+		      String sql = "SELECT * FROM moviedetail;";
+		      ResultSet val = stmt.executeQuery(sql);
+		      while(val.next())
+		      {
+		    	  System.out.println(val.getString(1));
+		      }
+		   }
+		   catch(Exception se)
+		   {
+			   se.printStackTrace();
+		   }
+	}
+}
